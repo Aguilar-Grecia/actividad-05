c93dbc9c2ffa91e1e569c71376ee2862ca007759
Proyectos de Pycharm <git@github.com:Aguilar-Grecia/PycharmProjects.git>
2025-07-11 10:21:11 -0600

    Primer commit

--- listadeestudiantes.py
+++ listadeestudiantes.py
@@ -4,3 +4,9 @@ class Estudiante:
         self.carne = carne
         self.carrera = carrera
         self.nota_final = nota_final
+    def __str__(self):
+        print(f"Nombre: {self.nombre}")
+        print(f"Carne: {self.carne}")
+        print(f"Carrera: {self.carrera}")
+        print(f"Nota final: {self.nota_final}")
+
